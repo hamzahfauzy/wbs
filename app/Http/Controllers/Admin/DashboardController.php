22c0cd39e52cdfe493b91225d8896350f01b384b
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-06-16 12:36:59 +0700

    notif on pengawas dan admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Faq;
+use App\Models\Admin;
+use App\Models\Wapiku;
+use Illuminate\Http\Request;
+use App\Models\NotifTemplate;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
@@ -29,6 +32,49 @@ class DashboardController extends Controller
             'replied_by_id' => $user->user_id,
             'messages'     => $request->messages
         ]);
+
+        $pengaduan = Pengaduan::find($pengaduan);
+
+        // user notif
+        $notif = NotifTemplate::where('send_to','user')->where('chat_baru','pengaduan_masuk')->first();
+        if($notif)
+        {
+            $message = $notif->template_text;
+
+            $message = str_replace('[nama]', $pengaduan->pengadu->nama, $message);
+            $message = str_replace('[alamat]', $pengaduan->pengadu->alamat, $message);
+            $message = str_replace('[nomor_hp]', $pengaduan->pengadu->nomor_hp, $message);
+            $message = str_replace('[judul]', $pengaduan->judul, $message);
+            $message = str_replace('[deskripsi]', $pengaduan->deskripsi, $message);
+            $message .= '\n\n_Ini adalah sistem notifikasi Whatsapp by: Dinas Kominfo Kabupaten Labuhanbatu Utara._';
+            Wapiku::send($nomor_hp, $message);
+        }
+
+        // admin notif
+        $notif = NotifTemplate::where('send_to','admin')->where('event_name','chat_baru')->first();
+        if($notif)
+        {
+            $message = $notif->template_text;
+
+            $message = str_replace('[nama]', $pengaduan->pengadu->nama, $message);
+            $message = str_replace('[alamat]', $pengaduan->pengadu->alamat, $message);
+            $message = str_replace('[nomor_hp]', $pengaduan->pengadu->nomor_hp, $message);
+            $message = str_replace('[judul]', $pengaduan->judul, $message);
+            $message = str_replace('[deskripsi]', $pengaduan->deskripsi, $message);
+            $message .= '\n\n_Ini adalah sistem notifikasi Whatsapp by: Dinas Kominfo Kabupaten Labuhanbatu Utara._';
+            $admin = Admin::get();
+            foreach($admin->pegawai as $pegawai)
+            {
+                $nomor_hp = preg_replace('/^0/','62',$pegawai->no_hp);
+                Wapiku::send($nomor_hp, $message);
+            }
+
+            foreach($admin->tks as $pegawai)
+            {
+                $nomor_hp = preg_replace('/^0/','62',$pegawai->no_hp);
+                Wapiku::send($nomor_hp, $message);
+            }
+        }
 
         return ['status'=>'success'];
     }
