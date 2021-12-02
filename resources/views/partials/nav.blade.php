7004adc9e7b9c7dd014ee42980f6f7a6d289d165
Hamzah Fauzy <hamzahfauzy97@gmail.com>
2021-12-02 16:41:18 +0700

    update nav to user

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -8,7 +8,7 @@
                 <span class="menu-title">Home</span>
                 </a>
             </li>
-            @if($user)
+            @if($user && in_array($user->id,[1,2]))
             <li class="nav-item">
                 <a class="nav-link" href="javascript:void(0)">
                 <i class="ti-bell menu-icon"></i>
